1138563b2893d33cf28720b7fd83911073280df9
EDGAR <krozcla00@gmail.com>
2026-09-07 11:42:48 -0500

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { Gallery } from './pages/gallery/gallery';
+import { Checkout } from './pages/checkout/checkout';
+import { Login } from './pages/login/login';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: Home, title: 'Inicio'},
+    {path: 'products', component: Products,  title: 'Productos'},
+    {path: 'gallery', component: Gallery, title: 'Galería'},
+    {path: 'checkout', component: Checkout, title: 'Carrito de compras'},
+    {path: 'login', component: Login, title: 'Iniciar sesión'},
+    {path: '**', component: NotFound, title: 'No encontrado'}
+];
